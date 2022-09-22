2d539dd63a2b95f72f09884c94835665aa2df19d
YeonwooSung <neos960518@gmail.com>
2022-09-22 11:03:48 +0900

    Add test message

--- server/chatbot.py
+++ server/chatbot.py
@@ -18,11 +18,13 @@ def as_json(f):
 
 class Chatbot(Resource):
     def get(self):
-        text = '안녕하세요!'
+        text = '안녕!'
         return self.run_chatbot(text)
 
     def run_chatbot(self, text):
         session_id = str(uuid.uuid4())
+        config_msg = f'project_id: {DialogflowConfig_TTS.project_id}, location: {DialogflowConfig_TTS.location}, agent_id: {DialogflowConfig_TTS.agent_id}, language_code: {DialogflowConfig_TTS.language_code}, session_id: {session_id}'
+        print(config_msg)
         response = detect_intent_texts(
             DialogflowConfig_TTS.project_id,
             DialogflowConfig_TTS.location,
@@ -35,4 +37,4 @@ class Chatbot(Resource):
     
     @as_json
     def genereate_json_with_utf8(self, input_data):
-        return {"결과": input_data} #, {'Content-Type': 'application/json; charset=utf-8'}
+        return {"결과": input_data}
