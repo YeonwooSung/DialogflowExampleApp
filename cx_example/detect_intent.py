2d17d2b9977645e81a630e417f2124f867333497
YeonwooSung <neos960518@gmail.com>
2022-09-22 11:01:20 +0900

    Update test code

--- cx_example/detect_intent.py
+++ cx_example/detect_intent.py
@@ -26,6 +26,7 @@ Examples:
 """
 
 import argparse
+from cgitb import text
 import uuid
 
 from google.cloud.dialogflowcx_v3beta1.services.agents import AgentsClient
@@ -49,6 +50,10 @@ def run_sample():
     language_code = "ko"
 
     detect_intent_texts(agent, session_id, texts, language_code)
+    
+    
+    for text in texts:
+        detect_intent_texts_test(project_id, location_id, agent_id, session_id, text, language_code)
 
 
 def detect_intent_texts(agent, session_id, texts, language_code):
@@ -80,6 +85,44 @@ def detect_intent_texts(agent, session_id, texts, language_code):
             " ".join(msg.text.text) for msg in response.query_result.response_messages
         ]
         print(f"Response text: {' '.join(response_messages)}\n")
+
+
+def detect_intent_texts_test(project_id, location_id, agent_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
+
+    session_path = f"{agent}/sessions/{session_id}"
+    print(f"Session path: {session_path}\n")
+    client_options = None
+    agent_components = AgentsClient.parse_agent_path(agent)
+    
+    location_id = agent_components["location"]
+    if location_id != "global":
+        api_endpoint = f"{location_id}-dialogflow.googleapis.com:443"
+        print(f"API Endpoint: {api_endpoint}\n")
+        client_options = {"api_endpoint": api_endpoint}
+    session_client = SessionsClient(client_options=client_options)
+
+    text_input = session.TextInput(text=text)
+    print(f'text: {text}')
+    print(f"Text Input: {text_input}")
+    query_input = session.QueryInput(text=text_input, language_code=language_code)
+    request = session.DetectIntentRequest(
+        session=session_path, query_input=query_input
+    )
+    response = session_client.detect_intent(request=request)
+
+    print("=" * 20)
+    print(f"Query text: {response.query_result.text}")
+    response_messages = [
+        " ".join(msg.text.text) for msg in response.query_result.response_messages
+    ]
+
+    response_text = ' '.join(response_messages)
+    print(f"Response text: {response_text}\n")
+    return response_text
 
 
 # [END dialogflow_cx_detect_intent_text]
